e4d550a65925e1c39b2dc8351b4212fc677d5d79
cirko33 <luka.ciric2106@gmail.com>
2024-01-14 18:01:47 +0100

    adjustments

--- kubernetes/up.sh
+++ kubernetes/up.sh
@@ -1,6 +1,9 @@
+kubectl apply -f configs/creds-config.yaml
+kubectl apply -f configs/postgres-config.yaml
 kubectl apply -f volumes.yaml
 kubectl apply -f deployments.yaml
 kubectl apply -f services.yaml
+sleep 1
 kubectl apply -f ingress.yaml
 
 clear
